9e8760a9a3846d44d5943f5893274d03a9536dc9
Shree33 <skbhanderi3@gmail.com>
2024-07-04 12:52:44 -0400

    clean up

--- config.ts
+++ config.ts
@@ -71,9 +71,9 @@ const config = {
     // subdomain to use when sending emails, if you don't have a subdomain, just remove it. Highly recommended to have one (i.e. mg.yourdomain.com or mail.yourdomain.com)
     subdomain: "mg",
     // REQUIRED — Email 'From' field to be used when sending magic login links
-    fromNoReply: `AutoTag <noreply@mg.shipfa.st>`,
+    fromNoReply: `AutoTag <noreply@autot.ag>`,
     // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
-    fromAdmin: `Shree at AutoTag <marc@mg.shipfa.st>`,
+    fromAdmin: `Shree at AutoTag <shree@autot.ag>`,
     // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
     supportEmail: "shree@mg.AutoT.ag",
     // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
