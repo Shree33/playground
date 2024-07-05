9caed67ff22c9dce50a13ca7d732be2228d90a70
Shree33 <skbhanderi3@gmail.com>
2024-07-05 16:36:07 -0400

    start dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,6 +11,15 @@ export default async function Dashboard() {
       <section className="max-w-xl mx-auto space-y-8">
         <ButtonAccount />
         <h1 className="text-3xl md:text-4xl font-extrabold">Private Page</h1>
+        <div class="bg-white p-4 rounded-lg">
+          <div class="relative bg-inherit">
+            <input type="text" id="prompt" name="prompt" class="peer bg-transparent h-10 w-100 rounded-lg text-gray-200 placeholder-transparent ring-2 px-10 mr-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600" placeholder="Enter Your Prompt"/>
+              <label for="username" class="absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-inherit mx-1 px-1 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all">
+                Enter Your Prompt
+              </label>
+            <button className="btn btn-primary" name="submit">Submit</button>
+          </div>
+        </div> 
       </section>
     </main>
   );
