054f3dbf45e7bf77f7c59f7ec9d45e7196fc6eef
Shree33 <skbhanderi3@gmail.com>
2024-07-05 17:21:30 -0400

    fix typing

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -4,52 +4,35 @@ import { cookies } from "next/headers";
 
 export const dynamic = "force-dynamic";
 
+export async function POST(req: any) {
+  const supabase = createRouteHandlerClient({ cookies });
+  const { data } = await supabase.auth.getSession();
+  const { session } = data;
 
-interface Request {
-    json(): Promise<any>;
-}
+  if (session) {
+    const body = await req.json();
 
-interface User {
-    email: string;
-}
-
-interface InsertResponse {
-    data: any;
-}
-
-interface ErrorResponse {
-    error: string;
-}
-
-export async function POST(req: Request): Promise<NextResponse> {
-    const supabase = createRouteHandlerClient({ cookies });
-    const { data } = await supabase.auth.getSession();
-    const { session } = data;
-
-    if (session) {
-        const body: User = await req.json();
-
-        if (!body.email) {
-            return NextResponse.json({ error: "Email is required" }, { status: 400 });
-        }
-
-        try {
-            // This call will fail if you haven't created a table named "users" in your database
-            const { data }: InsertResponse = await supabase
-                .from("users")
-                .insert({ email: body.email })
-                .select();
-
-            return NextResponse.json({ data }, { status: 200 });
-        } catch (e) {
-            console.error(e);
-            return NextResponse.json(
-                { error: "Something went wrong" },
-                { status: 500 }
-            );
-        }
-    } else {
-        // Not Signed in
-        return NextResponse.json({ error: "Not signed in" }, { status: 401 });
+    if (!body.email) {
+      return NextResponse.json({ error: "Email is required" }, { status: 400 });
     }
+
+    try {
+      // This call will fail if you haven't created a table named "users" in your database
+      const { data } = await supabase
+        .from("users")
+        .insert({ email: body.email })
+        .select();
+
+      return NextResponse.json({ data }, { status: 200 });
+    } catch (e) {
+      console.error(e);
+      return NextResponse.json(
+        { error: "Something went wrong" },
+        { status: 500 }
+      );
+    }
+  } else {
+    // Not Signed in
+    NextResponse.json({ error: "Not signed in" }, { status: 401 });
+  }
 }
